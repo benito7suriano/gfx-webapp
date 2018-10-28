a3870e07b23d257c5173d72f054375e6794adb55
Beno Suriano <bjsuriano@gmail.com>
2018-10-28 16:51:10 -0600

    Zona POST PUT and DELETE routes completed.

--- server/routes/api/zonas.js
+++ server/routes/api/zonas.js
@@ -28,3 +28,40 @@ router.get('/:zonaId', async (req, res, next) => {
   }
 })
 
+// POST /api/zona
+router.post('/', async (req, res, next) => {
+  try {
+    const zona = await Zona.create(req.body)
+    res.json(zona)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// PUT /api/zona/zonaId
+router.put('/:zonaId', async (req, res, next) => {
+  try {
+    const zonaId = req.params.zonaId
+    const zona = await Zona.findById(zonaId)
+
+    if (!zona) return res.sendStatus(404)
+
+    const updated = await zona.update(req.body)
+
+    res.status(200).json({ zona: updated, message: 'Updated successfully' }).end()
+  } catch (err) {
+    next(err)
+  }
+})
+
+// DELETE api/zona/zonaId
+router.delete('/:zonaId', async (req, res, next) => {
+  try {
+    const zonaId = req.params.zonaId
+    await Zona.destroy({ where: { id: zonaId } })
+    res.status(204).end()
+  } catch (err) {
+    next(err)
+  }
+})
+
